d8a8efa75d5e0094b20e41e6f06e33cddedd57d1
Dmitry <net.inkognito2@gmail.com>
2019-06-15 15:40:24 +0600

    Revert "Revert "1""
    This reverts commit c8b88572bde081b0f0cda4defc45a5665f383950.

---
+++ src/main/java/com/pfsc_server/repo/TypeOfFileRepo.java
@@ -0,0 +1,7 @@
+package com.pfsc_server.repo;
+
+import com.pfsc_server.domain.TypeOfFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypeOfFileRepo extends JpaRepository<TypeOfFile, Long> {
+}
