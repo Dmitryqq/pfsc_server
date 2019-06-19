a157939cff2b42984e00725d2408a86b9d5f37ef
atai <regingmandragon@gmail.com>
2019-06-20 00:07:00 +0600

    Добавлена проверка кол-ва файлов и размера файлов
    Co-Authored-By: regingman <regingman@users.noreply.github.com>

--- src/main/java/com/pfsc_server/controller/FileController.java
+++ src/main/java/com/pfsc_server/controller/FileController.java
@@ -9,11 +9,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -23,8 +27,6 @@ import java.util.List;
 @RequestMapping("file")
 public class FileController {
 
-    private static Logger LOG = LoggerFactory
-            .getLogger(FileController.class);
 
     private final FilesRepo fileRepo;
     private final ConfigsRepo configRepo;
@@ -57,42 +59,70 @@ public class FileController {
         return new ResponseEntity<>(file, HttpStatus.OK);
     }
 
+    /*@GetMapping("{id}")
+    public int fileCount(@PathVariable("id") Long fileId, @RequestParam("file") MultipartFile[] files) {
+        int count = fileRepo.countFiles(fileId) + files.length;
+        return count;
+    }*/
+
 
     @PostMapping
-    public ResponseEntity<ArrayList<File>> create(@RequestParam Long fileId, @RequestParam Long commitId, @RequestParam("file") MultipartFile[] files) throws IOException {
+    public ResponseEntity<List<File>> create(@RequestParam Long fileId, @RequestParam Long commitId, @RequestParam("file") MultipartFile[] files) throws IOException {
         if (fileId == null || commitId == null || files == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } else {
-            ArrayList<File> file = new ArrayList<>();
+            List<File> file = new ArrayList<>();
             Commit commit = commitsRepo.findById(commitId).orElse(null);
             TypeOfFile typeOfFile = typeOfFileRepo.findById(fileId).orElse(null);
             Config rootDir = configRepo.findById(1L).orElse(null);
-            if (rootDir == null || commit == null || typeOfFile == null) {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            } else {
-                java.io.File folders = new java.io.File(commit.getDir(rootDir.getValue()) + "\\" + typeOfFile.getName());
-                if(!folders.exists())
-                {
-                    folders.mkdir();
-                }
-                for (MultipartFile uploadedFile : files) {
-                    File tempFile = new File();
-                    tempFile.setCommit_id(commitId);
-                    tempFile.setFile_id(fileId);
-                    tempFile.setFile(typeOfFile);
-                    tempFile.setCommit(commit);
-                    String pathFile = commit.getDir(rootDir.getValue()) + "\\" + typeOfFile.getName() + "\\" + uploadedFile.getOriginalFilename();
-                    uploadedFile.transferTo(new java.io.File(pathFile));
-                    tempFile.setPath(pathFile);
-                    file.add(tempFile);
-                }
-                fileRepo.saveAll(file);
-                return new ResponseEntity<>(file, HttpStatus.CREATED);
-            }
 
+            File temp = new File();
+
+            int fileLength = files.length;
+            int count = fileRepo.countFiles(commitId) + fileLength;
+
+            if (count > typeOfFile.getMax_amount()) {
+                //превышен лимит кол-ва файлов
+                temp.setPath("Превышен лимит");
+                file.add(temp);
+                return new ResponseEntity<>(file, HttpStatus.NOT_FOUND);
+            } else {
+
+                if (rootDir == null || commit == null || typeOfFile == null) {
+                    temp.setPath("одно из полей пустое");
+                    file.add(temp);
+                    return new ResponseEntity<>(file, HttpStatus.NOT_FOUND);
+                } else {
+                    java.io.File folders = new java.io.File(commit.getDir(rootDir.getValue()) + "\\" + typeOfFile.getName());
+                    if (!folders.exists()) {
+                        folders.mkdir();
+                    }
+                    //необходимо сравнить файлы между собой на равенство
+                    //потом необходимо сравнить их на равенство между другими файлами из каталога
+
+                    for (MultipartFile uploadedFile : files) {
+                        if (uploadedFile.getSize() > typeOfFile.getMax_size() * 1024) {
+                            temp.setPath("размер файла превыщает допустимый");
+                            file.add(temp);
+                            return new ResponseEntity<>(file, HttpStatus.NOT_FOUND);
+                        } else {
+                            File tempFile = new File();
+                            tempFile.setCommit_id(commitId);
+                            tempFile.setFile_id(fileId);
+                            tempFile.setFile(typeOfFile);
+                            tempFile.setCommit(commit);
+                            String pathFile = commit.getDir(rootDir.getValue()) + "\\" + typeOfFile.getName() + "\\" + uploadedFile.getOriginalFilename();
+                            uploadedFile.transferTo(new java.io.File(pathFile));
+                            tempFile.setPath(pathFile);
+                            file.add(tempFile);
+                        }
+                    }
+                    fileRepo.saveAll(file);
+                    return new ResponseEntity<>(file, HttpStatus.CREATED);
+                }
+            }
         }
     }
-
 
 
     @PutMapping("{id}")
--- src/main/java/com/pfsc_server/repo/FilesRepo.java
+++ src/main/java/com/pfsc_server/repo/FilesRepo.java
@@ -2,6 +2,14 @@ package com.pfsc_server.repo;
 
 import com.pfsc_server.domain.File;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface FilesRepo extends JpaRepository<File, Long> {
+
+    //Требуется замена в будущем
+    @Query(value = "SELECT COUNT(*) FROM files u WHERE u.commit_id = :commit_id", nativeQuery = true)
+    int countFiles(Long commit_id);
+
 }
